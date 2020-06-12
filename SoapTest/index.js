5997a0b875ff3ec9bc70d90ceef59177aeadf2d8
boli <boli@sevadev.com>
2020-06-12 17:22:27 +0545

    ss

--- SoapTest/index.js
+++ SoapTest/index.js
@@ -1,32 +1,42 @@
-var soapx = require('soap');
+var soap = require('soap');
 
 module.exports = async function (context, req) {  
     context.log('JavaScript HTTP trigger function processed a request.');
     var wsdlUrl = 'http://www.dneonline.com/calculator.asmx?wsdl';
-    soapx.createClient(wsdlUrl, function (err, soapClient) {        
+    soap.createClient(wsdlUrl, function (err, soapClient) {        
         if (err) {
             context.res = {
                 status: 500,
                 body: err
             };
         } 
-        context.req = {
-            status: 200,
-            body: "aa"
-        }
-        soapClient.Add({
-            intA: 1,
-            intB: 2
-        }, function (err, result) {
-            if (err) {
+       
+        try{
+            soapClient.Add({
+                intA: 1,
+                intB: 2
+            }, function (err, result) {
+                if (err) {
+                    context.res = {
+                        status: 500,
+                        body: 'error while fetch data'
+                    };
+                }         
                 context.res = {
-                    status: 500,
-                    body: 'error while fetch data'
-                };
-            }         
+                    body: result
+                };                    
+            });
+        }catch(e){
             context.res = {
-                body: result
-            };                    
-        });
+                status: 200,
+                body: "catch error here"
+            }
+        }
     });  
+
+    context.res = {
+        status: 200,
+        body: 'print at the last line'
+    };
+
 }
