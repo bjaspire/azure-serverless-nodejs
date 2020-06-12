374061dc361d586ed1ef30892bed3f9e522c8934
boli <boli@sevadev.com>
2020-06-12 17:27:33 +0545

    soap fixed test

--- SoapTest/index.js
+++ SoapTest/index.js
@@ -3,40 +3,21 @@ var soap = require('soap');
 module.exports = async function (context, req) {  
     context.log('JavaScript HTTP trigger function processed a request.');
     var wsdlUrl = 'http://www.dneonline.com/calculator.asmx?wsdl';
-    soap.createClient(wsdlUrl, function (err, soapClient) {        
+    soap.createClient(wsdlUrl, function (err, soapClient) {
         if (err) {
-            context.res = {
-                status: 500,
-                body: err
-            };
-        } 
-       
-        try{
-            soapClient.Add({
-                intA: 1,
-                intB: 2
-            }, function (err, result) {
-                if (err) {
-                    context.res = {
-                        status: 500,
-                        body: 'error while fetch data'
-                    };
-                }         
-                context.res = {
-                    body: result
-                };                    
-            });
-        }catch(e){
+            context.log(err);
+        }      
+        soapClient.Add({
+            intA: 95,
+            intB: 99
+        }, function (err, result) {
+            if (err) {
+            context.log(err);
+            }
             context.res = {
                 status: 200,
-                body: "catch error here"
-            }
-        }
-    });  
-
-    context.res = {
-        status: 200,
-        body: 'print at the last line'
-    };
-
+                body: 'result try'
+            };
+        });
+    });
 }
